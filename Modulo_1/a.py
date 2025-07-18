dab52ba339be8d98aa73117a409638b6099581fa
juandiospvpnime <juandiospvp@gmail.com>
2025-07-18 13:40:36 -0400

    clase2

---
+++ Modulo_1/a.py
@@ -0,0 +1,13 @@
+puntuacion = int(input("cual es tu puntuacion"))
+if puntuacion >= 90:
+    print("bien hecho tu puntuacion es A")
+elif puntuacion >= 80 and puntuacion <= 89:
+    print("bien hecho tu puntuacion es D")
+elif puntuacion >= 70 and puntuacion <= 79:
+    print("bien hecho tu puntuacion es C") 
+elif puntuacion >= 60 and puntuacion <= 69:
+    print("bien hecho tu puntuacion es B")    
+elif puntuacion >= 50 and puntuacion <= 59:
+    print("bien hecho tu puntuacion es E")     
+else:
+    print("nesecitas mejorar")
---
+++ Modulo_1/clase-2.py
@@ -0,0 +1,13 @@
+nombre = input("¿Cuál es tu nombre? ")  
+edad = input("¿Cuántos años tienes? ")         
+pais = input("de que pais eres?")
+
+print(f"Hola, {nombre}! Tienes {edad} años.")  
+print(f"De donde eres?{pais}")
+print("¡Bienvenido al mundo de Python!")  
+
+edad_num = int(edad)
+print(f"Hola {nombre}, el año que viene tendrás {edad_num + 1} años y eres del pais {pais}.")
+
+pais = input("¿Cuál es tu pais? ")  
+print(f"Tu es pais ")  
